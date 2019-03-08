6e4cb108b549323accb30f4bedd0690ccda7ebd8
megan <megan@TOVAZ>
2019-03-08 14:37:48 -0600

    fix configuracion manager

---
+++ src/main/java/sitetech/hotspot/Modelos/ConfiguracionManager.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sitetech.hotspot.Modelos;
+
+import javafx.collections.ObservableList;
+import sitetech.Helpers.dbHelper;
+
+/**
+ *
+ * @author Willi
+ */
+public class ConfiguracionManager {
+    private dbHelper DbHelper;
+    
+    public ConfiguracionManager() {
+        DbHelper = new dbHelper();
+    }
+    
+    public Configuracion getConfiguracion(){
+        ObservableList<Configuracion> lista = (ObservableList<Configuracion>)DbHelper.Select("FROM Configuracion");
+        if (lista.size()>0)
+            return lista.get(0);
+        else
+            return null;
+    }
+    
+    public void Agregar(Configuracion conf)
+    {
+        DbHelper.Agregar(conf);
+    }
+    
+    public void Editar(Configuracion conf)
+    {
+        DbHelper.Editar(conf);
+    }
+}
