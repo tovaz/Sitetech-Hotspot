a085aff0fe328a65d6fde81e622750a9b270036a
williamvaldez@outlook.com <williamvaldez@outlook.com>
2019-03-13 07:50:51 -0600

    Temas fix recover  -

---
+++ src/main/java/sitetech/hotspot/Modelos/ConfiguracionManager2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sitetech.hotspot.Modelos;
+
+import javafx.collections.ObservableList;
+import sitetech.Helpers.dbHelper;
+
+/**
+ *
+ * @author Willi
+ */
+public class ConfiguracionManager2 {
+    private dbHelper DbHelper;
+    
+    public ConfiguracionManager2() {
+        DbHelper = new dbHelper();
+    }
+    
+    public Configuracion getConfiguracion(){
+        ObservableList<Configuracion> lista =   (ObservableList<Configuracion>)DbHelper.Select("FROM Configuracion WHERE id=1");
+        if (lista.size() == 0)
+            return new Configuracion(true);
+        else
+            return (Configuracion)lista.get(0);
+    }
+    
+    public static Configuracion getConfiguracion(dbHelper db){
+        ObservableList<Configuracion> lista =   (ObservableList<Configuracion>)db.Select("FROM Configuracion WHERE id=1");
+        if (lista.size() == 0)
+            return new Configuracion(true);
+        else
+            return (Configuracion)lista.get(0);
+    }
+    
+    public void Agregar(Configuracion conf)
+    {
+        DbHelper.Agregar(conf);
+    }
+    
+    public void Editar(Configuracion conf)
+    {
+        DbHelper.Editar(conf);
+    }
+}
