8f7c52b2748f3e2e534dc8666beff0767f1bfc85
megan <megan@TOVAZ.mshome.net>
2019-03-25 13:49:32 -0600

    Estilo scroll bar css

--- src/main/java/sitetech/Helpers/LabelHelper.java
+++ src/main/java/sitetech/Helpers/LabelHelper.java
@@ -1,36 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sitetech.Helpers;
-
-import com.sun.javafx.scene.control.skin.LabeledText;
-import com.sun.javafx.scene.text.TextLayout;
-import com.sun.javafx.tk.Toolkit;
-import java.awt.FontMetrics;
+import javafx.geometry.Bounds;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextBoundsType;
 
-//@SuppressWarnings("restriction")
 public class LabelHelper{
-  public static boolean checkWith(Label label, String texto){
-      FontMetrics fm;
-      int width = fm.stringWidth(texto);
-      return width > label.getWidth();
+  public static boolean necesitaTooltip(Font font, Label label){
+    Text text = new Text(label.getText());
+    text.setFont(font);
+    Bounds tb = text.getBoundsInLocal();
+    Rectangle stencil = new Rectangle(
+            tb.getMinX(), tb.getMinY(), tb.getWidth(), tb.getHeight()
+    );
+
+    Shape intersection = Shape.intersect(text, stencil);
+
+    Bounds ib = intersection.getBoundsInLocal();
+    return ib.getWidth() > label.getPrefWidth();
   }
   
   public static void asignarTexto(Label label, String texto){
-      
       label.setText(texto);
-      
-      LabeledText tx = (LabeledText ) label.getClip();
-      System.err.println("TEXTO: " + tx);
-      if ()
-    //if (label.isNeedsLayout())
-        label.setTooltip(new Tooltip(texto));
+      if (necesitaTooltip(label.getFont(), label)){
+          Tooltip tp = new Tooltip(texto);
+          
+        label.setTooltip(tp);
+      }
   }
 }
